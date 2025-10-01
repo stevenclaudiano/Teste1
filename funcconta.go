90c5b3e8a518bb3a5e9dfd1e5afe74280f45e3b7
stevenclaudiano <steven.claudiano@route66.com.br>
2025-10-01 17:45:41 -0300

    func retorna 2 ou mais result

---
+++ funcconta.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func somar(teste string, n1 int, n2 int) (int, string) {
+	return n1 + n2, teste
+}
+
+func main() {
+	nome, soma1 := somar("Steven", 10, 10)
+	fmt.Println(nome, soma1)
+}
