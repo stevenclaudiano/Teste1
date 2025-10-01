d12e6bd8783bce577a3f235986ae68a9c667ef2c
stevenclaudiano <steven.claudiano@route66.com.br>
2025-10-01 14:40:06 -0300

    For simples

---
+++ forSimples.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i < 10; i++ {
+		mostrarcasa1()
+	}
+
+}
+
+func mostrarcasa1() {
+
+	fmt.Println("Casa Grande")
+	fmt.Println("Casa Pequena")
+}
